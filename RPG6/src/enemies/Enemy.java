c05ec975a3cb2a37f12e37aaba3cbf88948b294f
Teacher <Teacher@10.148.46.98>
2016-01-22 09:26:42 -0500

    added determineTarget method to Enemy class

--- RPG6/src/enemies/Enemy.java
+++ RPG6/src/enemies/Enemy.java
@@ -1,5 +1,13 @@
 package enemies;
 
-public class Enemy {
+import java.util.ArrayList;
 
+public abstract class Enemy {
+
+	public Enemy(){
+		//TODO
+	}
+	
+	public abstract Character determineTarget(ArrayList<Character> character);
+	
 }
