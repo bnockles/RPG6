37a00f7a7f0ab414b4d0fb83357804076b55edde
Student6 <Student6@10.8.33.154>
2016-02-11 12:12:27 -0500

    Added a constructor to Item

--- RPG6/src/items/Item.java
+++ RPG6/src/items/Item.java
@@ -1,5 +1,37 @@
 package items;
 
-public class Item {
-	//does this work
+import java.awt.image.BufferedImage;
+
+public abstract class Item {
+	
+	String description;
+	String name;
+	boolean durable;
+	BufferedImage itemImage;
+	//hi
+	
+	public Item(String description, String name, BufferedImage image, boolean durable){
+		this.description = description;
+		this.name = name;
+		this.itemImage = image;
+		this.durable = durable;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isDurable() {
+		return durable;
+	}
+
+	public BufferedImage getItemImage() {
+		return itemImage;
+	}
+
+	public abstract void onUse();
 }
