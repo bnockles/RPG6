c41d66c41f39d20317759f68fa4138c93b687625
Jeff <jlei9099@bths.edu>
2016-02-11 12:18:06 -0500

    test

--- RPG6/src/towns/Storable.java
+++ RPG6/src/towns/Storable.java
@@ -1,5 +1,17 @@
 package towns;
 
+import java.util.ArrayList;
+
 public interface Storable {
-	//test
+	
+	public ArrayList<String> storedItems = new ArrayList<String>();
+	
+	public void storeItem(Item item)//{
+//		if(item != questItem){
+//			playerInventory.remove(item);
+//			storedItems.add(item);
+//		}
+//	}
+	
+	public void storageFee(ArrayList<String> list, int days);
 }
