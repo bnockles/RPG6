9a0c010a2af85ea53330048e89d3f9254c5d56ab
Jlau8844 <jlau8844@bths.edu>
2016-02-11 13:17:35 -0500

    Does this work

--- RPG6/src/enemies/Boss.java
+++ RPG6/src/enemies/Boss.java
@@ -2,6 +2,13 @@ package enemies;
 
 
 public class Boss extends Enemy{
-
+  
+  int hp = character.getAttack *5
+  int attack = (character.getHealth * ((double)Math.random))/4
+  
+  public Boss(int health, int attack, int expGiven){
+    
+    
+  }
 
 }
