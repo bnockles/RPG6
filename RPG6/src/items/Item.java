cfa5a75b7b52cb95e0f118030375602ee3a47b8a
Student6 <Student6@10.8.33.142>
2016-02-11 12:59:24 -0500

    added functionality

--- RPG6/src/items/Item.java
+++ RPG6/src/items/Item.java
@@ -1,25 +1,25 @@
 package items;
 
-import java.awt.image.BufferedImage;
-
 public abstract class Item {
-	private String description;
 	private String name;
+	private String description;
 	//BufferedImage itemImage;
 	//hi
-	
-	public Item(String description, String name){
+
+	public Item(String name, String description){
 		this.description = description;
 		this.name = name;
 	}
 	
-	public String getDescription() {
-		return description;
-	}
+	public String toString(){ return this.name; }
+	public boolean equals(Item item){ return this.name == item.name; }
+	
+	public String getDescription() { return this.description; }
+	public String getName() { return this.name; }
+	
+	
 
-	public String getName() {
-		return name;
-	}
-
-	public abstract void onUse();
+	public abstract boolean canUse(Character user);
+	public abstract void use(Character user);
+	
 }
