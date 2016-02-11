154a41c970abdd975e7270ade5a330001c15a0c7
Harry <Harry@Harry-VAIO>
2016-02-10 22:07:50 -0500

    Added code to interface Marketable and class Market

--- RPG6/src/towns/Market.java
+++ RPG6/src/towns/Market.java
@@ -1,5 +1,10 @@
 package towns;
 
-public class Market {
-
+public abstract class Market {
+	
+	abstract void itemBuy(Item item);
+	// method used for buying item
+	
+	abstract void itemSell(Item item);
+	// method used for selling item
 }
--- RPG6/src/towns/Marketable.java
+++ RPG6/src/towns/Marketable.java
@@ -2,4 +2,9 @@ package towns;
 
 public interface Marketable {
 	
+	void checkBuy(Item item);
+	// checks to see if item can be bought 
+	
+	void checkSell(Item item);
+	// checks to see if item can be sold
 }
