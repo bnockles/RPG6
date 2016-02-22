9174893b7543c39fc8808b37203fd214922fb241
Student6 <Student6@10.8.33.106>
2016-02-22 12:24:44 -0500

    Thought I did this before

--- RPG6/src/items/effect.java
+++ RPG6/src/items/effect.java
@@ -1,5 +1,29 @@
 package items;
 
 public abstract class effect {
+	String ailment;
+	int buff;
+	int damage;
+	
+	
+	public effect (String ailment, int buff, int damage) {
+		this.ailment=ailment;
+		this.buff=buff;
+		this.damage=damage;
+	}
 
+
+	public String getAilment() {
+		return ailment;
+	}
+
+
+	public int getBuff() {
+		return buff;
+	}
+
+
+	public int getDamage() {
+		return damage;
+	}
 }
