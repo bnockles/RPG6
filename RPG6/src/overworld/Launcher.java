6f991285f6df4bdffdee4b4d664779860e4ba4d8
yoits <yoits@DESKTOP-SJGUKMB.78B430.nycboe.org>
2016-02-23 22:38:35 -0800

    Code done together by both Jesse and Joe

---
+++ RPG6/src/overworld/Launcher.java
@@ -0,0 +1,13 @@
+package overworld;
+
+import FirstRpg.Display.Display;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game=new Game("First Rpg", 800, 600);
+		game.start();
+	}
+
+}
