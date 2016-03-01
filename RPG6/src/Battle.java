2fe8920861535833f190346fcb8a573c39f7cc63
bma7228 <bma7228@bths.edu>
2016-02-29 19:12:53 -0500

    Update Battle.java

--- RPG6/src/Battle.java
+++ RPG6/src/Battle.java
@@ -31,6 +31,12 @@ public class Battle {
 			defender.setHealth(defender.getHealth()-damageToBeDealt);
 		}
 		
+		public static int getLevelExp(int lv){
+			//returns the EXP needed to reach a level
+			int exp = (int)(.1*(Math.pow(lv+1, 3.5)));
+			return exp;
+		}
+		
 		/**
 		 * need fields: double expRequiredToLevel, double currentExp
 		 * Hero and Enemy extend Char
@@ -38,14 +44,25 @@ public class Battle {
 		 * @return sets hero exp to current+amount enemy gives
 		 */
 		public static void giveExp(Hero hero, Enemy enemy){
-			if(hero.getCurrentExp()+enemy.getExpGained() > hero.getExpNeededToLevel()){
+			hero.setCurrentExp((hero.getCurrentExp()+enemy.getExpGained()));
+			if(hero.getCurrentExp() > hero.getExpNeededToLevel()){
 				hero.levelUp();
-				hero.setCurrentExp((hero.getCurrentExp()+enemy.getExpGained())-hero.getExpNeededToLevel());
-			}else{
-				hero.setCurrentExp(hero.getCurrentExp()+enemy.getExpGained());
 			}
 		}
 		
+		public static int getExpNeededToLevel(Hero hero){
+			//returns the difference between the EXP to reach the next level and the characters current EXP
+			return getLevelExp(hero.level+1)-hero.getCurrentExp();
+		}
+		
+		public static void levelUp(Hero hero){
+			//updates character statistics when reaching a new level
+			while(hero.getCurrentExp() >= getLevelExp(hero.level)){
+				hero.level++;
+				hero.health = hero.health + (int)(hero.level*1.8);
+				//fill in other stats
+			}
+		}
 		/**
 		 * 
 		 * @param c character who is attempting to dodge
