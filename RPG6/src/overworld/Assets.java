a9510ede82fe43086b53d6432a4d3fb98e188ff9
Jesse <Jesse@WINDOWS-CP4SA59>
2016-03-02 21:44:50 -0600

    j

--- RPG6/src/overworld/Assets.java
+++ RPG6/src/overworld/Assets.java
@@ -6,7 +6,7 @@ import java.awt.image.BufferedImage;
 //import FirstRpg.gfx.SpriteSheet;
 
 public class Assets {
-	//JOE
+	//JOEe
 	private static final int pWidth=32, pHeight=48;
 	private static final int tWidth=32, tHeight=32;
 	
