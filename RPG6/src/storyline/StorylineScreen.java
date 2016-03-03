d154168a08b4c02cabd827a1b02faf1a22d6ae28
thussain6730 <thussain6730@bths.edu>
2016-03-03 02:02:30 -0500

    Update StorylineScreen.java
    added a callimage() which is like a backgorund changer which is like out mini cutscrenes gottta add that repaint but first i put those calls - Anna

--- RPG6/src/storyline/StorylineScreen.java
+++ RPG6/src/storyline/StorylineScreen.java
@@ -1,76 +1,111 @@
-package demo;
-// import directors;
+package storyline;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+import directors.Game;
+import directors.Screen;
+import directors.UtilityMethods;
 /**
  * 
- * @author Tasnia
+ * @author Nockles witj bits of Annamation 
  *
  */
 public class StorylineScreen extends Screen implements KeyListener {
-	public static String message = "How would you like to proceed? Press L or R";
-	public static String keyCode = "";
-//	@Override
-	public void paintScreen(Graphics2D g2) {
-		g2.setColor(Color.white);
+
+	StoryPoint storyPoint;
+	int decisionPoint;
+	Bufferimage backgroundScreen;
+	
+	/**
+	 * @param game
+	 */
+	public StorylineScreen(Game game) {
+		super(game);
+		decisionPoint=1;
+		storyPoint = new StoryPoint();
 		
-		int height;
-		int width;
-		g2.fillRect(0, 0, width, height);
-		g2.setColor(Color.black);
-		//rest of code
-		try {
-			g2.drawString(message, 50, 125);
-			g2.drawString("Go left", 50, 100);
-			g2.drawString("Go right", 50, 150);
-			if (keyCode = KeyPress.VK_L){
-				Event.burnObject("building");
-			}
-			if (keyCode = KeyPress.VK_R){
-				Event.floodObject("House");
-			}
-		}
 	}
-//	@Override
-	public KeyListener getKeyListener() {
-		return this;
+
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+
+	public void keyPressed(KeyEvent e) {
+		int k = e.getKeyCode();
+		if(k==KeyEvent.VK_SPACE){
+			if(storyPoint.nextLn()){
+				nextDescision();
+			};
+		}
+		if(k==KeyEvent.VK_1){
+			storyPoint.select(0);
+			storyPoint.update();
+			
+		}
+		if(k==KeyEvent.VK_2){
+			storyPoint.select(1);
+			storyPoint.update();
+//			storyPoint.nextLn();;
+			
+		}
+		if(k==KeyEvent.VK_Q){
+			nextDescisionAuto();
+		}
+
+	}
+
+
+	private void nextDescisionAuto() {
+		storyPoint.select(0);
+		decisionPoint = storyPoint.getNextPoint();
+		storyPoint=new StoryPoint(storyPoint.getEvents(), decisionPoint);
+	}
+
+
+	private void nextDescision() {
+//		storyPoint.select(0);
+		decisionPoint = storyPoint.getNextPoint();
+		storyPoint=new StoryPoint(storyPoint.getEvents(), decisionPoint);
+	}
+
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void paintScreen(Graphics2D g2) {
+		g2.setColor(Color.green);
+		g2.fillRect(0, 0, width, height);
+		try{
+			g2.drawImage(storyPoint.getImage(), 0, 0,null);
+		}catch(Exception e){
+			
+		}
 	}
 	
-	public void keyPressed(KeyPress e) {
-		if(e.getKeyCode()==KeyPress.VK_L){
-			System.out.println("You chose to go left.");
-			keyCode = e.getKeyCode();
-			update();
-			game.repaint();
-		}
-		if(e.getKeyCode()==KeyPress.VK_R){
-			System.out.println("You chose to go right");
-			keyCode = e.getKeyCode();
-			update();
-			game.repaint();
-		}
+	public BufferedImage getImage(){
+		return image;
 	}
-	public void keyReleased(KeyPress e) {
-		// TODO Auto-generated method stub
+	
+	public void callImage(int imageNum){
+		// assuming we ever get those images on here 
+		backgroundImage = UtilityMethods.getImageFromFile(this, "/storyline/sample/"+ imageNum +".png");
+	}
+	
 
-	}
-
-	public void keyTyped(KeyPress e) {
-		// TODO Auto-generated method stub
-
-	}
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	@Override
+	public KeyListener getKeyListener() {
+		return this;
 	}
 }
