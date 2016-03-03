c773da9fa6382f15ffa349b210af875e6029fb0c
Carmen <jruan1402@bths.edu>
2016-03-02 22:48:56 -0500

    Changes Idk

--- RPG6/src/BattleEffects.java
+++ RPG6/src/BattleEffects.java
@@ -1,28 +1,29 @@
 import java.util.Timer;
 
-public class BattleEffects{ 
-
-	int timer = new Timer(3600,this);
+public class BattleEffects {
+	static Timer timer = new Timer();
 	timer.run();
 
 	public static void counter(){}
 	public static void run(){
 		for(int i=timer ; i>=0; i-=1){
-			if(i==0) //player automatically uses attack
+			if(i==0){
+				//player automatically uses attack
 			}
-		}	
+		}
+	}	
 
-	Player HP;
+	static Player HP;
 	int newHP = new Player HP/2; 
 
 	public static void poisonChance(){
 		int poison = (int)(Math.random() * ((100) +1)); // range from 1 to 100
-		if(int poison > 50){
+		if(poison > 50){
 			HP -= 20;
 		}
 	}
 	
-	boolean escape = true; 
+	static boolean escape = true; 
 
 	public static boolean escapeChance(){
 		if (newHP < HP){
@@ -33,5 +34,5 @@ public class BattleEffects{
 		}
 		return escape;
 	}
-}
 
+}
