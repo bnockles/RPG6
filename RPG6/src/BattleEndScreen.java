7f9b567034337ffc17d2c927b9cd24836b2baf9e
Michael Li <Mli7785@bths.edu>
2016-03-03 00:50:22 -0500

    .

--- RPG6/src/BattleEndScreen.java
+++ RPG6/src/BattleEndScreen.java
@@ -1,50 +1,72 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import directors.Game;
 import directors.Screen;
 
-public class BattleEndScreen extends Screen{
-	public BattleEndScreen(Game game) {
-		super(game);
-		// TODO Auto-generated constructor stub
-	}
-	private int inventoryIdx = Math.random() * Inventory.size();
+public class BattleEndScreen extends Screen implements KeyListener{
+	Hero hero;
+	Enemy enemy;
+	Item item;
+	int battleGold;
+	private ArrayList<Item> inventory;
 	
-	private ArrayList<Item> Inventory = new ArrayList<Item>();
-	
- 	public void win(Hero hero, Enemy enemy, int level ,Item item,double getExpGained, double currentExp,int battleGold){
- 		public void paintScreenWin(Graphics g){
- 			g.drawString("Current Level:" +level, 30, 65);  
- 			g.drawString("Experience Earned: "+enemy.getExpGained(), 30,75);
- 			g.drawString("Current Exp:"+ hero.getCurrentExp(), 30, 85);
- 			g.drawString("Gold Gained: "+ battleGold,30,95);
- 			g.drawString("Item Obtained: "+ item, 30, 105);
- 		
- 	
- 		}
+	public BattleEndScreen(Hero hero, Enemy enemy,int battleGold,Game game){
+ 		super(game);
+ 		this.hero = hero;
+ 		this.enemy = enemy;
+ 		this.battleGold = battleGold;
  	}
-	public void lose(Hero hero, Enemy enemy, int level ,Item item,double getExpGained, double currentExp,int battleGold){
-		public void paintScreenLose(Graphics g2){
- 			g2.drawString("Current Level:" +level, 30, 65);  
- 			g2.drawString("Experience Lost: "+enemy.getExpGained(), 30,75);
- 			g2.drawString("Current Exp:"+ hero.getCurrentExp(), 30, 85);
- 			g2.drawString("Gold Lost: "+ battleGold,30,95);
- 		
-		
+	
+	//private int inventoryIdx = (int)Math.random() * Inventory.size();
+	
+	
+ 	public void paintScreenWin(Graphics g){
+			g.drawString("Current Level:" + hero.getLevel(), 30, 65);  
+			g.drawString("Experience Earned: "+enemy.getExp(), 30,75);
+			g.drawString("Current Exp:"+ hero.getExp(), 30, 85);
+			g.drawString("Gold Gained: "+ battleGold,30,95);
+			g.drawString("Item Obtained: "+ item, 30, 105);
 	}
+ 	
+	public void paintScreenLose(Graphics g2){
+ 			g2.drawString("Current Level:" +hero.getLevel(), 30, 65);  
+ 			g2.drawString("Experience Lost: "+enemy.getExp(), 30,75);
+ 			g2.drawString("Current Exp:"+ hero.getExp(), 30, 85);
+ 			g2.drawString("Gold Lost: "+ battleGold,30,95);
+	}
+	
 		public void paintScreen(Graphics2D g2) {
 			// TODO Auto-generated method stub
 			g2.setColor(Color.white);
 			g2.fillRect(0, 0, width, height);
 			g2.setColor(Color.black);
 		}
-		
+
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
 		public KeyListener getKeyListener() {
 			// TODO Auto-generated method stub
-			return null;
+			return this;
 		}
+
+		
 	
 }
