53bce9897217778fd936a8e7d310cb51d943170b
Willson <Willson@Willson-PC.fios-router.home>
2016-03-06 16:18:40 -0500

    added value to all pots

--- RPG6/src/items/Consumable.java
+++ RPG6/src/items/Consumable.java
@@ -2,11 +2,21 @@ package items;
 
 public class Consumable extends Item {
 	int amount;
-	public Consumable(String name, String description, String req, int amount) {
+	int value;
+	public Consumable(String name, String description, String req, int amount, int value) {
 		super(name, description, req);
 		this.amount = amount;
+		this.value = value;
 	}
 	
+	public int getAmount() {
+		return amount;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 	public boolean canUse(Character user){
 		return true;
 	}
