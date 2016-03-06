8d7daace9191ce20b140aed3143652941d6725ae
Willson <Willson@Willson-PC.fios-router.home>
2016-03-05 22:34:15 -0500

    added descriptions for all made items

--- RPG6/src/items/equipStock.java
+++ RPG6/src/items/equipStock.java
@@ -91,109 +91,109 @@ public class equipStock {
 			return new Equipment("Wooden Wand", "REQ: 5 INT", "This wooden wand can cast very basic skills without breaking", 5, new Stats(5, 0, 0, 5, 5, 1), 100);
 		}
 		if (type == M_WD){
-			return new Equipment("Mithril Wand", "REQ: 15 INT", "", 5, new Stats(15, 0, 0, 15, 10, 3), 100);
+			return new Equipment("Mithril Wand", "REQ: 15 INT", "This mithril wand is forged by the goddess's of this planet.", 5, new Stats(15, 0, 0, 15, 10, 3), 100);
 		}
 		if (type == F_WD){
-			return new Equipment("Fairy's Wand", "REQ: 35 INT, 20 WIS", "", 5, new Stats(50, 0, 0, 35, 24, 5), 100);
+			return new Equipment("Fairy's Wand", "REQ: 35 INT, 20 WIS", "Fairies inherited their magic into this wand", 5, new Stats(50, 0, 0, 35, 24, 5), 100);
 		}
 		if (type == A_WD){
-			return new Equipment("Angel's Wand", "REQ: 75 INT, 50 WIS", "", 5, new Stats(80, 0, 0, 80, 60, 10), 100);
+			return new Equipment("Angel's Wand", "REQ: 75 INT, 50 WIS", "Reincarnated Angels crafted their souls into this very object.", 5, new Stats(80, 0, 0, 80, 60, 10), 100);
 		}
 		if (type == E_SF){
-			return new Equipment("Emerald Staff", "REQ: 10 INT", "", 5, new Stats(8, 0, 0, 10, 10, 1), 100);
+			return new Equipment("Emerald Staff", "REQ: 10 INT", "Putting an emerald magic stone onto a stick. Very clever.", 5, new Stats(8, 0, 0, 10, 10, 1), 100);
 		}
 		if (type == R_SF){
-			return new Equipment("Emerald Staff", "REQ: 45 INT", "", 5, new Stats(40, 0, 0, 50, 40, 5), 100);
+			return new Equipment("Emerald Staff", "REQ: 45 INT", "Harnesses the powers of the fire ruby.", 5, new Stats(40, 0, 0, 50, 40, 5), 100);
 		}
 		if (type == Q_SF){
-			return new Equipment("Queen's Staff", "REQ: 75 INT", "", 5, new Stats(70, 0, 0, 75, 50, 10), 100);
+			return new Equipment("Queen's Staff", "REQ: 75 INT", "From the queen herself. Forged by the servants of the queen.", 5, new Stats(70, 0, 0, 75, 50, 10), 100);
 		}
 		if (type == S_SF){
-			return new Equipment("Sun Goddess's Staff", "REQ: 100 INT", "", 5, new Stats(90, 0, 0, 100, 65, 20), 100);
+			return new Equipment("Sun Goddess's Staff", "REQ: 100 INT", "With the power of a thousand suns. The Sun Goddess's Staff is the stronget of its kind", 5, new Stats(90, 0, 0, 100, 65, 20), 100);
 		}
 		if (type == W_SR){
-			return new Equipment("Wooden Spear", "REQ: 5 STR", "", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
+			return new Equipment("Wooden Spear", "REQ: 5 STR", "This wooden spear will break when it hits any type of armor.", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
 		}
 		if (type == G_SR){
-			return new Equipment("Golden Spear", "REQ: 20 STR", "", 5, new Stats(20, 20, 3, 0, 0, 3), 100);
+			return new Equipment("Golden Spear", "REQ: 20 STR", "A spear made of gold. Stronger than it looks.", 5, new Stats(20, 20, 3, 0, 0, 3), 100);
 		}
 		if (type == B_SR){
-			return new Equipment("Battle Axe Spear", "REQ: 50 STR", "", 5, new Stats(50, 50, 5, 0, 0, 5), 100);
+			return new Equipment("Battle Axe Spear", "REQ: 50 STR", "Equipped with a battle axe at the end of the pole. Scary.", 5, new Stats(50, 50, 5, 0, 0, 5), 100);
 		}
 		if (type == SR_D){
-			return new Equipment("Spear of Destruction", "REQ: 99 STR", "", 5, new Stats(120, 100, 10, 1, 1, 10), 100);
+			return new Equipment("Spear of Destruction", "REQ: 99 STR", "Harnesses the dark powers to invoke world destruction.", 5, new Stats(120, 100, 10, 1, 1, 10), 100);
 		}
 		if (type == G_HR){
-			return new Equipment("Golden Hammer", "REQ: 5 STR", "", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
+			return new Equipment("Golden Hammer", "REQ: 5 STR", "Harder than steel. A hammer that will serve it's purpose", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
 		}
 		if (type == M_HR){
-			return new Equipment("Mithril Hammer", "REQ: 15 STR", "", 5, new Stats(15, 15, 3, 0, 0, 3), 100);
+			return new Equipment("Mithril Hammer", "REQ: 15 STR", "Forged with fascinating gems. Adds a twist of powers", 5, new Stats(15, 15, 3, 0, 0, 3), 100);
 		}
 		if (type == D_HR){
-			return new Equipment("Diamond Hammer", "REQ: 35 STR", "", 5, new Stats(35, 35, 5, 0, 0, 5), 100);
+			return new Equipment("Diamond Hammer", "REQ: 35 STR", "Hardest of all hammers. Truly outrageous.", 5, new Stats(35, 35, 5, 0, 0, 5), 100);
 		}
 		if (type == P_HR){
-			return new Equipment("Poppy's Hammer", "REQ: 70 STR", "", 5, new Stats(70, 70, 10, 0, 0, 10), 100);
+			return new Equipment("Poppy's Hammer", "REQ: 70 STR", "Poppy's Hammer. It has lived through thousands of wars. An ancient killing weapon.", 5, new Stats(70, 70, 10, 0, 0, 10), 100);
 		}
 		if (type == W_SD){
-			return new Equipment("Wooden Shield", "REQ: 5 STR", "", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
+			return new Equipment("Wooden Shield", "REQ: 5 STR", "It can defend against a snail... Probably?", 5, new Stats(5, 5, 1, 0, 0, 1), 100);
 		}
 		if (type == I_SD){
-			return new Equipment("Iron Shield", "REQ: 15 STR", "", 5, new Stats(15, 15, 3, 0, 0, 3), 100);
+			return new Equipment("Iron Shield", "REQ: 15 STR", "Forged with decent materials. It will serve it's purpose; for the most part.", 5, new Stats(15, 15, 3, 0, 0, 3), 100);
 		}
 		if (type == T_WL){
-			return new Equipment("Tank's Will", "REQ: 30 STR", "", 5, new Stats(30, 30, 5, 0, 0, 5), 100);
+			return new Equipment("Tank's Will", "REQ: 30 STR", "Brings out the will of the tank. Protects with passion.", 5, new Stats(30, 30, 5, 0, 0, 5), 100);
 		}
 		if (type == GS_D){
-			return new Equipment("God's Shield", "REQ: 100 STR", "", 5, new Stats(100, 100, 10, 10, 10, 10), 100);
+			return new Equipment("God's Shield", "REQ: 100 STR", "An undestructible shield. God forged it himself.", 5, new Stats(100, 100, 10, 10, 10, 10), 100);
 		}
 		if (type == W_CE){
-			return new Equipment("Wooden Chestplate", "REQ: NONE", "", 2, new Stats(10, 10, 10, 10), 100);
+			return new Equipment("Wooden Chestplate", "REQ: NONE", "Blocks from any incoming attack, partially.", 2, new Stats(10, 10, 10, 10), 100);
 		}
 		if (type == I_CE){
-			return new Equipment("Iron Chestplate", "REQ: 15 STR", "", 2, new Stats(20, 20, 20, 20), 100);
+			return new Equipment("Iron Chestplate", "REQ: 15 STR", "Guards the wearer from all physical attacks.", 2, new Stats(20, 20, 20, 20), 100);
 		}
 		if (type == D_CE){
-			return new Equipment("Diamond Chestplate", "REQ: 40 STR", "", 2, new Stats(40, 40, 30, 30), 100);
+			return new Equipment("Diamond Chestplate", "REQ: 40 STR", "Hard as diamond. Well it is diamond. Very sturdy.", 2, new Stats(40, 40, 30, 30), 100);
 		}
 		if (type == D_AR){
-			return new Equipment("Devil's Armor", "REQ: 999 STR", "", 2, new Stats(500, 500, 99, 99), 100);
+			return new Equipment("Devil's Armor", "REQ: 999 STR", "Armor made of darkness. Anyone who wears this will die. Anyone who doesn't is a devil.", 2, new Stats(500, 500, 99, 99), 100);
 		}
 		if (type == L_PT){
-			return new Equipment("Leather Pants", "REQ: 5 STR", "", 3, new Stats(5, 5, 0, 0), 100);
+			return new Equipment("Leather Pants", "REQ: 5 STR", "Pants of a leather. Sticks to the skin.", 3, new Stats(5, 5, 0, 0), 100);
 		}
 		if (type == S_PT){
-			return new Equipment("Silk Pants", "REQ: 15 STR", "", 3, new Stats(15, 15, 1, 1), 100);
+			return new Equipment("Silk Pants", "REQ: 15 STR", "Soft but protective.", 3, new Stats(15, 15, 1, 1), 100);
 		}
 		if (type == GW_P){
-			return new Equipment("God Woven Pants", "REQ: 50 STR", "", 3, new Stats(50, 55, 3, 3), 100);
+			return new Equipment("God Woven Pants", "REQ: 50 STR", "God made these pants himself. They will never break.", 3, new Stats(50, 55, 3, 3), 100);
 		}
 		if (type == F_LG){
-			return new Equipment("Forsaken Leggings", "REQ: 100 STR", "", 3, new Stats(100, 100, 5, 5), 100);
+			return new Equipment("Forsaken Leggings", "REQ: 100 STR", "Pants forbidden from this world. It's surging with power.", 3, new Stats(100, 100, 5, 5), 100);
 		}
 		if (type == S_HT){
-			return new Equipment("Straw Hat", "REQ: 5 STR", "", 1, new Stats(5, 5, 1, 1), 100);
+			return new Equipment("Straw Hat", "REQ: 5 STR", "Looks like a pirate will wear this hat. Old but fashionable.", 1, new Stats(5, 5, 1, 1), 100);
 		}
 		if (type == I_HT){
-			return new Equipment("Iron Helmet", "REQ: 20 STR", "", 1, new Stats(20, 20, 3, 3), 100);
+			return new Equipment("Iron Helmet", "REQ: 20 STR", "Helmet that covers the whole face. For the sake of protection.", 1, new Stats(20, 20, 3, 3), 100);
 		}
 		if (type == P_HT){
-			return new Equipment("Plat Helmet", "REQ: 55 STR", "", 1, new Stats(55, 50, 5, 5), 100);
+			return new Equipment("Plat Helmet", "REQ: 55 STR", "It's heavy. It is probably not a good idea to wear this if you have a squishy head.", 1, new Stats(55, 50, 5, 5), 100);
 		}
 		if (type == S_HD){
-			return new Equipment("Satan's Headguard", "REQ: 500 STR", "", 1, new Stats(500, 500, 500, 500), 100);
+			return new Equipment("Satan's Headguard", "REQ: 500 STR", "Stolen from Satan. Any human who wears this will turn into a devil.", 1, new Stats(500, 500, 500, 500), 100);
 		}
 		if (type == R_SE){
-			return new Equipment("Running Shoes", "REQ: 5 STR", "", 4, new Stats(5, 5, 1, 1), 100);
+			return new Equipment("Running Shoes", "REQ: 5 STR", "Shoes that make you run faster.", 4, new Stats(5, 5, 1, 1), 100);
 		}
 		if (type == R_BT){
-			return new Equipment("Rain Boots", "REQ: 15 STR", "", 4, new Stats(15, 15, 5, 5), 100);
+			return new Equipment("Rain Boots", "REQ: 15 STR", "Boots that help you in the rain.", 4, new Stats(15, 15, 5, 5), 100);
 		}
 		if (type == A_SE){
-			return new Equipment("Armor Shoes", "REQ: 45 STR", "", 4, new Stats(45, 45, 10, 10), 100);
+			return new Equipment("Armor Shoes", "REQ: 45 STR", "Shoes that protect your feet. It helps protect the wearer generally too.", 4, new Stats(45, 45, 10, 10), 100);
 		}
 		if (type == G_SL){
-			return new Equipment("God's Sandals", "REQ: 100 STR", "", 4, new Stats(100, 100, 100, 100), 100);
+			return new Equipment("God's Sandals", "REQ: 100 STR", "God wears these sandals. They look weak and old, but they help a lot.", 4, new Stats(100, 100, 100, 100), 100);
 		}
 		return null;
 	}
--- RPG6/src/items/potionStock.java
+++ RPG6/src/items/potionStock.java
@@ -19,46 +19,46 @@ public class potionStock {
 	
 	public static Consumable consumableStock(int type){
 		if(type == HP_100){
-			return new Consumable("HP 100", "Restores 100 HP to the target.", "" , 10);
+			return new Consumable("HP 100", "Restores 100 HP to the target.", "A red potion. Small but interesting" , 10);
 		}
 		if(type == HP_500){
-			return new Consumable("HP 500", "Restores 500 HP to the target.", "" , 2);
+			return new Consumable("HP 500", "Restores 500 HP to the target.", "It's the size of your hand. Looks like it will take a while to drink." , 2);
 		}
 		if(type == HP_1000){
-			return new Consumable("HP 1000", "Restores 1000 HP to the target.", "" , 1);
+			return new Consumable("HP 1000", "Restores 1000 HP to the target.", "This looks like it's enough for a whole family, but it's for one person." , 1);
 		}
 		if(type == MP_100){
-			return new Consumable("MP 100", "Restores 100 MP to the target.", "" , 10);
+			return new Consumable("MP 100", "Restores 100 MP to the target.", "A blue potion. Small but intriguing" , 10);
 		}
 		if(type == MP_500){
-			return new Consumable("MP 500", "Restores 500 MP to the target.", "" , 2);
+			return new Consumable("MP 500", "Restores 500 MP to the target.", "The liquid is glowing. It'll be weird to drink this now." , 2);
 		}
 		if(type == MP_1000){
-			return new Consumable("MP 1000", "Restores 1000 MP to the target.", "" , 1);
+			return new Consumable("MP 1000", "Restores 1000 MP to the target.", "It looks like snail juice; A huge jug of it. Appetizing indeed." , 1);
 		}
 		if(type == PT_RES){
-			return new Consumable("PARTY RESTORE", "Restores everyone in your party full HP", "" , 99);
+			return new Consumable("PARTY RESTORE", "Restores everyone in your party full HP", "All in one potion. It's small but it will do everyone justice." , 99);
 		}
 		if(type == ATK_I){
-			return new Consumable("ATK I", "Gives target +5 attack", "" , 3);
+			return new Consumable("ATK I", "Gives target +5 attack", "A quick boost. Looks like a pill of some sort." , 3);
 		}
 		if(type == ATK_II){
-			return new Consumable("ATK II", "Gives target +15 attack", "" , 1);
+			return new Consumable("ATK II", "Gives target +15 attack", "A sharp needle. Instructions: Inject for everlasting pleasure." , 1);
 		}
 		if(type == DEF_I){
-			return new Consumable("DEF I", "Gives target +5 defense", "" , 3);
+			return new Consumable("DEF I", "Gives target +5 defense", "A quick aid. It's a non-appetizing brown color." , 3);
 		}
 		if(type == DEF_II){
-			return new Consumable("DEF II", "Gives target +15 defense", "" , 1);
+			return new Consumable("DEF II", "Gives target +15 defense", "A sharp needle. Instructions: Inject for a nice hard body." , 1);
 		}
 		if(type == SPD_I){
-			return new Consumable("SPD I", "Gives target +5 speed", "" , 3);
+			return new Consumable("SPD I", "Gives target +5 speed", "A quick speed. It's red and white like a normal pill." , 3);
 		}
 		if(type == SPD_II){
-			return new Consumable("SPD II", "Gives target +15 speed", "" , 1);
+			return new Consumable("SPD II", "Gives target +15 speed", "A sharp needle. Instructions: Inject to be a ninja." , 1);
 		}
 		if(type == ARROWS){
-			return new Consumable("ARROWS", "Quiver of arrows. Good with a bow", "" , 6);
+			return new Consumable("ARROWS", "Arrow itself: 5 ATK; With Bow: 15 ATK", "Quiver of arrows. Good with a bow", 6);
 		}
 		return null;
 	}
