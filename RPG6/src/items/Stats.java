7a1eb73714ee93d570147b907c099d8c93a1b41e
Willson <Willson@Willson-PC.fios-router.home>
2016-03-05 23:09:29 -0500

    added parts to stats. need help with this later

--- RPG6/src/items/Stats.java
+++ RPG6/src/items/Stats.java
@@ -18,13 +18,19 @@ public final class Stats {
 		this(0, 0, 0, 0, 0);
 	}
 	public Stats(int hp, int mp, int atk, int def){
-		this(hp, mp, atk, def, 0, 0, 0, 0, 0);
+		this(hp, mp, atk, def, 0, 0, 0, 0, 0); //stats for characters
 	}
 	public Stats(int str, int dex, int iq, int wis, int luck){
-		this(0, 0, 0, 0, str, dex, iq, wis, luck);
+		this(0, 0, 0, 0, str, dex, iq, wis, luck); //stats for armor and character + armor together
 	}
 	public Stats(int atk, int str, int dex, int iq, int wis, int luck){
-		this(0, atk, 0, 0, str, dex, iq, wis ,luck);
+		this(0, 0, atk, 0, str, dex, iq, wis ,luck); //stats for weapons
+	}
+	public Stats(int atk, int hp, int str, int dex, int iq, int wis, int luck){
+		this(hp, 0, atk, 0, str, dex, iq, wis, luck); //stats for shields
+	}
+	public Stats(int hp, int def, int str){
+		this(hp, 0, 0, 0, str, 0, 0, 0, 0); //stats for str armor
 	}
 	public Stats(int hp, int mp, int atk, int def, int str, int dex, int iq, int wis, int luck){
 		this.health 		= hp;
