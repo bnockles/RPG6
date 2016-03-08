889a54f0ffe5d59d6c257b71eb261a1fca66f6af
Carmen <jruan1402@bths.edu>
2016-03-07 23:39:42 -0500

    Carmen thingy

--- RPG6/src/Attacks.java
+++ RPG6/src/Attacks.java
@@ -40,4 +40,9 @@ public class Attacks {
 //Attacks attacks1 = new Attacks("Barrier Aerial", 70, 20, 10);
 //Attacks attacks2 = new Attacks("Sand Slash Storm", 60, 10, 15);
 //Attacks attacks3 = new Attacks("Dance of Fairies", 65, 15, 17);
-//Attacks attacks4 = new Attacks("Imperial Salvation", 85, 30, 40);
+//Attacks Attacks4 = new Attacks(“Shock Wave”, 35, 25, 18);
+//Attacks Attacks5 = new Attacks("Geyser Cut”, 40, 20, 10);
+//Attacks Attacks6 = new Attacks(“Demon Scythe”, 65, 20, 0);
+//Attacks Attacks7 = new Attacks(“Purifier”, 60, 17, 16);
+//Attacks Attacks8 = new Attacks(“Vampiric Bite”, 20, 15, 50);
+//Attacks attacks9 = new Attacks("Imperial Engage", 85, 30, 40);
