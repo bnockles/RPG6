9db1d490973a9fe4caf0ae0f92321a2e88ac3f5e
Jeff <jlei9099@bths.edu>
2016-05-23 13:18:06 -0400

    New stuff

--- RPG6/src/towns/Storable.java
+++ RPG6/src/towns/Storable.java
@@ -1,28 +1,8 @@
 package towns;
 
-import java.util.ArrayList;
-
 public interface Storable {
-	
-	/**
-	 * 
-	 * @author Jeffrey Lei
-	 *
-	 */
-	
-	public ArrayList<String> storedItems = new ArrayList<String>();
-	
-	public void storeItem(String item);//{
-//		if(item != questItem){
-//			playerInventory.remove(item);
-//			storedItems.add(item);
-//		}
-//	}
-	
-	public void storageFee(ArrayList<String> list, int days);//{
-//		int fee = days*days*(list.size());
-//		playerCurrency = playerInventory.getMoney()-fee;
-//	}
-	
-	public void retrieveItem(String item); 
+
+	public boolean isStorable();
+
+	public int getRarity(Item i);
 }
--- RPG6/src/towns/Storage.java
+++ RPG6/src/towns/Storage.java
@@ -1,21 +1,81 @@
 package towns;
 
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 
 public class Storage extends AbstractNPC {
+	private Item[][] storage;
 
 	public Storage(String nm, String gen, String des, boolean talkable) {
 		super(nm, gen, des, talkable);
-		// TODO Auto-generated constructor stub
+		storage = new Item[3][3];
 	}
-	
-	static ArrayList<String> storedItems = new ArrayList<String>();
-	static boolean storable;
-	
+
+	/**
+	 * Moves item(s) from player inventory to storage
+	 * 
+	 * @param playerInventory 	Where item is being taken out of
+	 * @param storage			New location of item
+	 */
+	public static void storeItem(Item[][] playerInventory, Item[][] storage, 
+			Item toBeStored, Storable s){
+		if(s.isStorable()){
+			int count = 0;
+			for(int r = 0; r < storage.length; r++){
+				for(int c = 0; c < storage[0].length; c++){
+					if(storage[r][c] != null){
+						count++;
+						if(count >= (storage.length*storage[0].length))
+							System.out.println("Your storage is full!!!");
+					}else{
+						storage[r][c] = toBeStored;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Calculates how much player will pay upon withdrawal of item(s)
+	 * 
+	 * @param storage
+	 */
+	public static int storageFee(Item[][] storage, Storable j){
+		int fee = 0;
+		for(Item[] r : storage){
+			for(Item i : r)
+				fee += j.getRarity(i);
+		}
+		return fee/(storage.length);
+	}
+
+	/**
+	 * Moves item(s) from storage to player inventory
+	 * 
+	 * @param storage
+	 * @param playerInventory
+	 */
+	public static void retrieveItem(Item[][] storage, Item[][] playerInventory, int r, int c){
+		if(r < 0 || r > 3){
+			if(c < 0 || c > 3){
+			}
+		}else{
+			for(int i = 0; i < playerInventory.length; i++){
+				for(int j = 0; j < playerInventory[0].length; j++){
+					if(playerInventory[i][j] != null){
+					}else{
+						playerInventory[i][j] = storage[r][c];
+					}
+				}
+			}
+		}
+	}
+
+	public Item[][] getStorage(){
+		return storage;
+	}
+
 	@Override
 	public void npcMovement() {  
 		// TODO Auto-generated method stub
-		
 	}
 }
