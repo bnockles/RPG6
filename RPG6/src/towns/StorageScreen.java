f99177c4e8ac322a0395e0bb49dbfbb795f5ead0
Jeffrey Lei <jlei9099@bths.edu>
2016-05-26 01:17:40 -0400

    Last update for the night

--- RPG6/src/towns/StorageScreen.java
+++ RPG6/src/towns/StorageScreen.java
@@ -1,5 +1,6 @@
 package towns;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,7 +11,7 @@ import directors.Screen;
 
 
 public class StorageScreen extends Screen implements KeyListener{
-	
+
 	public static final int storeItem1 = KeyEvent.VK_A;
 	public static final int storeItem2 = KeyEvent.VK_S;
 	public static final int storeItem3 = KeyEvent.VK_D;
@@ -24,38 +25,39 @@ public class StorageScreen extends Screen implements KeyListener{
 	public static final int right = KeyEvent.VK_RIGHT;
 	public static final int enter = KeyEvent.VK_ENTER;
 	public static final int quit = KeyEvent.VK_Q;
-			
+
 	public static String welcomeMessage = "Welcome to your wares!";
 	public static String actionMessage = "What would you like to do today? " +
-			"Press 4 to Store Items and 5 to Retrieve Items";
+			" Press 4 to Store Items and 5 to Retrieve Items";
 	public static String storedItems = "";
-	
+
 	Storage storageNPC;
 	SampleCharacter sNPC;
-	
+
 	Item e1;
 	Item e2;
 	Item e3;
 	Item e4;
-	
+
 	int counter = 0;
-	
+	boolean storing = false;
+
 	public ArrayList<Item> playerInventory;
-	
+
 	public StorageScreen(Game game) {
 		super(game);
-		
+
 		sNPC = new SampleCharacter("/images/storage.png",50,50,"npc");
-		
+
 		storageNPC = new Storage("Greg","Male","He is your storage guy, him and his cousins (who look exactly like him) "
 				+ "are in every town in case you want to store items and then take them back for later.", true);
-	
+
 		playerInventory = new ArrayList<Item>(); 
 		e1 = equipStock.equipStocks(0);
 		e2 = equipStock.equipStocks(1);
 		e3 = equipStock.equipStocks(2);
 		e4 = equipStock.equipStocks(3);
-		
+
 		playerInventory.add(e1);
 		playerInventory.add(e2);
 		playerInventory.add(e3);
@@ -64,45 +66,56 @@ public class StorageScreen extends Screen implements KeyListener{
 
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == quit){
+			System.out.println("Q was pressed");
 			actionMessage = "What would you like to do today?" +
 					"Press 4 to Store Items and 5 to Retrieve Items";
+			update();
 		}
 		if(e.getKeyCode() == store){
 			actionMessage = "Move the left or right arrow keys to choose what to store! Press k to quit.";
 			System.out.println("4 was pressed");
+			storing = true;
+			update();
+		}
+		
+		if(storing){
 			if(e.getKeyCode() == left && counter != 0){
 				System.out.println("Left arrow key was pressed");
 				counter--;
+				update();
+				game.repaint();
 			}
 			if(e.getKeyCode() == right && counter != playerInventory.size()){
 				System.out.println("Right arrow key was pressed");
 				counter++;
+				update();
+				game.repaint();
 			}
 		}
-		
+
 	}
 
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void paintScreen(Graphics2D g2) {
-		
+
 		g2.drawImage(sNPC.getImage(), sNPC.getX(), sNPC.getY(), null);
-		
+
 		//displays greetings
 		g2.drawString(welcomeMessage, 200, 100);
-		
+
 		//displays commands
 		g2.drawString(actionMessage, 200, 150);
-		
+
 		//displays storage inventory
 		g2.drawString("Storage Inventory: ",200, 200);
 		int xAx = 300;
@@ -112,7 +125,7 @@ public class StorageScreen extends Screen implements KeyListener{
 				xAx += 100;				
 			}
 		}
-		
+
 		//displays player inventory
 		g2.drawString("Player Inventory: ", 200, 300);
 		int xAxis = 300;
@@ -121,6 +134,12 @@ public class StorageScreen extends Screen implements KeyListener{
 			xAxis += 100;
 		}
 		
+		if(storing == true){
+			for(Item g:playerInventory){
+				g2.setColor(Color.blue);
+			}
+		}
+
 	}
 
 	@Override
