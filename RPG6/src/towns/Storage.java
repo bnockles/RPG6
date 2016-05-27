47d0c65848f9840e40221f394ac7aa0c84fa0ee5
Jeff <jlei9099@bths.edu>
2016-05-27 12:05:40 -0400

    Last update

--- RPG6/src/towns/Storage.java
+++ RPG6/src/towns/Storage.java
@@ -3,46 +3,35 @@ package towns;
 import java.util.ArrayList;
 
 public class Storage extends AbstractNPC{
-	private static Equipment[] storage;
+	private static Item[] storage;
 
 	public Storage(String nm, String gen, String des, boolean talkable) {
 		super(nm, gen, des, talkable);
-		storage = new Equipment[3];
+		storage = new Item[3];
 	}
 
-	/**
-	 * Moves item(s) from player inventory to storage
-	 * 
-	 * @param toBeStored 	Item that is going to be stored
-	 * @param storage		New location of item
-	 */
-	public static void storeItem(int index, Equipment toBeStored){
-		int count = 0;
-		for(int r = 0; r < storage.length; r++){
-				if(storage[index] != null){
-					count++;
-					if(count >= (storage.length))
-						System.out.println("Your storage is full!!!");
-				}else{
-					storage[index] = toBeStored;
-					System.out.println("Item has been stored!");
-				}
+	//stores items
+	public static ArrayList<Item> storeItem(int index, Item item, ArrayList<Item> inventory){
+		if(storage[index] != null){
+			inventory.add(storage[index]);
 		}
+		storage[index] = item;
+		inventory.remove(item);
+		System.out.println("Item has been stored!");
+		return inventory;
 	}
 	
-	public static ArrayList<Equipment> convertToList(Equipment[] array){
-		ArrayList<Equipment> list = new ArrayList<Equipment>();
-			for(Equipment e : array){
-				list.add(e);
+	//retrieves items
+	public static ArrayList<Item> retrieveItem(int index, Item item, ArrayList<Item> inventory){
+		if(storage[index] != null){
+			inventory.add(storage[index]);
+			storage[index] = null;
 		}
-		return list;
+		System.out.println("Item has been retrieved");
+		return inventory;
 	}
 
-	/**
-	 * Calculates how much player will pay upon withdrawal of item(s)
-	 * 
-	 * @param storage
-	 */
+	//calculates fee
 	public static int storageFee(Item[][] storage, Storable j){
 		int fee = 0;
 		for(Item[] r : storage){
@@ -52,29 +41,9 @@ public class Storage extends AbstractNPC{
 		return fee/(storage.length);
 	}
 
-	/**
-	 * Moves item(s) from storage to player inventory
-	 * 
-	 * @param storage
-	 * @param playerInventory
-	 */
-	public static void retrieveItem(Item[][] storage, Item[][] playerInventory, int r, int c){
-		if(r < 0 || r > 3){
-			if(c < 0 || c > 3){
-			}
-		}else{
-			for(int i = 0; i < playerInventory.length; i++){
-				for(int j = 0; j < playerInventory[0].length; j++){
-					if(playerInventory[i][j] != null){
-					}else{
-						playerInventory[i][j] = storage[r][c];
-					}
-				}
-			}
-		}
-	}
 
-	public static Equipment[] getStorage(){
+
+	public static Item[] getStorage(){
 		return storage;
 	}
 
--- RPG6/src/towns/StorageScreen.java
+++ RPG6/src/towns/StorageScreen.java
@@ -1,5 +1,6 @@
 package towns;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,7 +11,7 @@ import directors.Screen;
 
 
 public class StorageScreen extends Screen implements KeyListener{
-	
+
 	public static final int storeItem1 = KeyEvent.VK_A;
 	public static final int storeItem2 = KeyEvent.VK_S;
 	public static final int storeItem3 = KeyEvent.VK_D;
@@ -20,66 +21,243 @@ public class StorageScreen extends Screen implements KeyListener{
 	public static final int slot3 = KeyEvent.VK_3;
 	public static final int store = KeyEvent.VK_4;
 	public static final int retrieve = KeyEvent.VK_5;
-		
-	//test
+	public static final int left = KeyEvent.VK_LEFT;
+	public static final int right = KeyEvent.VK_RIGHT;
+	public static final int enter = KeyEvent.VK_ENTER;
+	public static final int quit = KeyEvent.VK_Q;
+	public static final int retrieveItem = KeyEvent.VK_G;
+
 	public static String welcomeMessage = "Welcome to your wares!";
 	public static String actionMessage = "What would you like to do today? " +
-			"4 to Store Items and 5 to Retrieve Items";
-	public static String storedItems = "";
+			" Press 4 to Store Items and 5 to Retrieve Items";
+	public static String quitMessage = "Press Q to quit interaction at anytime.";
+
+	public static int selectedIndex = 0;
+	public static int selectedStorageIndex = 0;
+
+	Storage storageNPC;
+	SampleCharacter sNPC;
+
 	Item e1;
 	Item e2;
 	Item e3;
 	Item e4;
 	
-	public ArrayList<Item> list;
+	int counter = 0;
+	int storageCounter = 0;
 	
+	boolean storing = false;
+	boolean retrieving = false;
+	boolean selectingSlot = false;
+
+	public ArrayList<Item> playerInventory;
+
 	public StorageScreen(Game game) {
 		super(game);
-	
-		list = new ArrayList<Item>(); 
+
+		sNPC = new SampleCharacter("/images/storage.png",50,50,"npc");
+
+		storageNPC = new Storage("Greg","Male","He is your storage guy, him and his cousins (who look exactly like him) "
+				+ "are in every town in case you want to store items and then take them back for later.", true);
+
+		playerInventory = new ArrayList<Item>(); 
 		e1 = equipStock.equipStocks(0);
 		e2 = equipStock.equipStocks(1);
 		e3 = equipStock.equipStocks(2);
 		e4 = equipStock.equipStocks(3);
-		
-		list.add(e1);
-		list.add(e2);
-		list.add(e3);
-		list.add(e4);
+
+		playerInventory.add(e1);
+		playerInventory.add(e2);
+		playerInventory.add(e3);
+		playerInventory.add(e4);
 	}
 
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == quit){
+			System.out.println("Q was pressed");
+			actionMessage = "What would you like to do today?" +
+					" Press 4 to Store Items and 5 to Retrieve Items";
+			storing = false;
+			retrieving = false;
+			update();
+		}
+		if(e.getKeyCode() == store){
+			actionMessage = "Move the left or right arrow keys to choose what to store! Press enter to start storing.";
+			System.out.println("4 was pressed");
+			storing = true;
+			update();
+		}
+		if(storing){
+			if(e.getKeyCode() == left){
+				System.out.println("Left arrow key was pressed");
+				counter--;
+				if(counter < 0){
+					counter = playerInventory.size() - 1;
+				}
+				update();
+				game.repaint();
+			}
+			if(e.getKeyCode() == right){
+				System.out.println("Right arrow key was pressed");
+				counter++;
+				if(counter > playerInventory.size() - 1){
+					counter = 0;
+				}
+				update();
+				game.repaint();
+			}
+			selectedIndex = counter;
+			if(e.getKeyCode() == enter){
+				selectingSlot = !selectingSlot;
+				System.out.println("Enter was pressed");
+				if(selectingSlot){
+					actionMessage = "Press 1 to store in slot 1, 2 to store in slot 2 and 3 to store in slot 3.";
+				}else{
+					actionMessage = "What would you like to do today?" +
+							"Press 4 to store items and 5 to retrieve items";
+					storing = false;
+				}
+			}
+			if(selectingSlot){
+				if(e.getKeyCode() == slot1){
+					System.out.println("Picking slot 1");
+					if(storing){
+						playerInventory = Storage.storeItem(0, playerInventory.get(selectedIndex), playerInventory);
+					}
+				}
+				if(e.getKeyCode() == slot2){
+					System.out.println("Picking slot 2");
+					if(storing){
+						playerInventory = Storage.storeItem(1, playerInventory.get(selectedIndex), playerInventory);
+					}
+				}
+				if(e.getKeyCode() == slot3){
+					System.out.println("Picking slot 3");
+					if(storing){
+						playerInventory =Storage.storeItem(2, playerInventory.get(selectedIndex), playerInventory);
+					}
+				}
+			}
+		}
+		if(e.getKeyCode() == retrieve){
+			for(int i = 0; i < Storage.getStorage().length; i++){
+				if(Storage.getStorage()[i] != null){
+					actionMessage = "Move the left or right arrow keys to choose what to retrieve! Press G to start retrieving.";
+					System.out.println("5 was pressed");
+					retrieving = true;
+					update();
+					break;
+				}
+			}
+		}
+		if(retrieving){
+			if(e.getKeyCode() == left){
+				System.out.println("Left arrow key was pressed");
+				storageCounter--;
+				if(storageCounter < 0){
+					storageCounter = Storage.getStorage().length - 1;
+				}
+				update();
+				game.repaint();
+			}
+			if(e.getKeyCode() == right){
+				System.out.println("Right arrow key was pressed");
+				storageCounter++;
+				if(storageCounter > Storage.getStorage().length - 1){
+					storageCounter = 0;
+				}
+				update();
+				game.repaint();
+			}
+			selectedStorageIndex = storageCounter;
+			
+			if(e.getKeyCode() == retrieveItem){
+				playerInventory = Storage.retrieveItem(selectedStorageIndex, Storage.getStorage()[selectedStorageIndex], playerInventory);
+			}
+		}
 		
+
 	}
 
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void paintScreen(Graphics2D g2) {
-		// TODO Auto-generated method stub
-		int yAxis = 100;
-		
-		//displays player inventory
-		for(Item i:list){
-			g2.drawString(i.getItemName(),100, yAxis);
-			yAxis += 100;
+		//displays npc
+		g2.drawImage(sNPC.getImage(), sNPC.getX(), sNPC.getY(), null);
+
+		//displays greetings
+		g2.drawString(welcomeMessage, 200, 100);
+
+		//displays quit
+		g2.drawString(quitMessage, 200, 150);
+		//displays commands
+		g2.drawString(actionMessage, 200, 200);
+
+		//displays storage inventory
+		g2.drawString("Storage Inventory: ",200, 250);
+		int xAx = 300;
+		for(int i = 0; i < Storage.getStorage().length; i++){
+			if(i == selectedStorageIndex && retrieving){
+				if(Storage.getStorage()[i] != null && i == selectedStorageIndex){
+					g2.setColor(Color.blue);
+					g2.drawString(Storage.getStorage()[i].getItemName(), xAx, 250);
+				}else if(Storage.getStorage()[i] != null){
+					g2.setColor(Color.black);
+					g2.drawString(Storage.getStorage()[i].getItemName(), xAx, 250);
+				}else{
+					g2.setColor(Color.black);
+					g2.drawString(" _____" , xAx, 250);
+				}
+			}else{
+				if(Storage.getStorage()[i] != null){
+					g2.setColor(Color.black);
+					g2.drawString(Storage.getStorage()[i].getItemName(), xAx, 250);
+				}else{
+					g2.setColor(Color.black);
+					g2.drawString(" _____" , xAx, 250);
+				}
+			}
+			g2.setColor(Color.black);
+			xAx += 100;
 		}
-		
+//		for(Item s:Storage.getStorage()){
+//			if(s != null){
+//				g2.drawString(s.getItemName(), xAx, 250);
+//				xAx += 100;				
+//			}else{
+//				g2.drawString(" _____" , xAx, 250);
+//				xAx += 100;
+//			}
+//		}
+
+		//displays player inventory
+		g2.drawString("Player Inventory: ", 200, 300);	
+		int xAxis = 300;
+		for(int i = 0; i < playerInventory.size(); i++){
+			if(i == selectedIndex && storing){
+				g2.setColor(Color.blue);
+				g2.drawString(playerInventory.get(i).getItemName(),xAxis, 300);
+			}else{
+				g2.setColor(Color.black);
+				g2.drawString(playerInventory.get(i).getItemName(),xAxis, 300);
+			}
+			xAxis += 100;
+		}
+
 	}
 
 	@Override
 	public KeyListener getKeyListener() {
-		// TODO Auto-generated method stub
-		return null;
+		return this;
 	}
 
 }
